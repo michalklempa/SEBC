7e8610f1a8a89843c6c413f0e2c9185c92475f35
Michal Klempa <michal.klempa@gmail.com>
2016-09-22 15:23:34 +0200

    YARN testing done

--- resources/tools/YARNtest.sh
+++ resources/tools/YARNtest.sh
@@ -3,18 +3,19 @@
 
 HADOOP_MR=/opt/cloudera/parcels/CDH/lib/hadoop-0.20-mapreduce
 HADOOP_PATH=/opt/cloudera/parcels/CDH/bin
+LOGFILE=${HOME}/TG.log
 
 # Mark start of the loop
 echo Testing loop started on `date`
 
 # Mapper containers
-for i in 2    
+for i in 2 50 100 400 800
 do
    # Reducer containers
-   for j in 2 
+   for j in 2 50 100 500
    do                 
       # Container memory
-      for k in 512 1024 
+      for k in 512 1024 2048 4096
       do                         
          # Set mapper JVM heap 
          MAP_MB=`echo "($k*0.8)/1" | bc` 
@@ -22,13 +23,14 @@ do
          # Set reducer JVM heap 
          RED_MB=`echo "($k*0.8)/1" | bc` 
 
-        $HADOOP_PATH/hadoop jar $HADOOP_MR/hadoop-examples.jar teragen \
+	echo "Mappers: ${i} Reducers: {j} Container memory: {k}" >> ${LOGFILE}
+        time -a ${LOGFILE} $HADOOP_PATH/hadoop jar $HADOOP_MR/hadoop-examples.jar teragen \
                      -Dmapreduce.job.maps=$i \
                      -Dmapreduce.map.memory.mb=$k \
                      -Dmapreduce.map.java.opts.max.heap=$MAP_MB \
-                     100000 /results/tg-10GB-${i}-${j}-${k} 1>tera_${i}_${j}_${k}.out 2>tera_${i}_${j}_${k}.err                       
+                     1000000 /results/tg-10GB-${i}-${j}-${k} 1>tera_${i}_${j}_${k}.out 2>tera_${i}_${j}_${k}.err                       
 
-       $HADOOP_PATH/hadoop jar $HADOOP_MR/hadoop-examples.jar terasort \
+       $time -a ${LOGFILE} HADOOP_PATH/hadoop jar $HADOOP_MR/hadoop-examples.jar terasort \
                      -Dmapreduce.job.maps=$i \
                      -Dmapreduce.job.reduces=$j \
                      -Dmapreduce.map.memory.mb=$k \
